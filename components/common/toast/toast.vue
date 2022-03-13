008674d519766c5d4fcbe38d24cd1f16c1608fe5
yangqinglin <ouyangqinglinloveme@gmail.com>
2022-03-13 10:19:30 +0800

    H5抱名页整体配置完成

---
+++ components/common/toast/toast.vue
@@ -0,0 +1,94 @@
+<template>
+<transition name="trans">
+  <div class="common-fixed-toast" v-show="display">
+    <div class="common-fixed-toast-mask" v-show="mask"></div>
+    <div class="common-fixed-toast-msg" :class="{sp: type}" :style="style">
+      <i class="common-fixed-toast-icon" v-show="type"></i>
+      <div class="common-fixed-toast-content" v-html="content"></div>
+    </div>
+  </div>
+</transition>
+</template>
+
+<script>
+export default {
+  name: "common-fixed-toast",
+  data () {
+    return {
+      display: false, // {{ boolean }} 显示
+      mask: true, // {{ boolean }} 遮罩
+      content: '', // {{ string }} 显示内容
+      timer: null, // {{ number }} 计时器
+      style: null, // {{ number }} 样式
+      type: '', // {{ string }} 图标类型 tick / cross / warn
+    }
+  },
+  methods: {
+    show ({ content = '', time = 2000, style = {}, type = '', mask = false } = {}) {
+      clearTimeout(this.timer)
+      this.display = true
+      this.content = content
+      this.style = style
+      this.type = type
+      this.mask = mask
+      this.timer = setTimeout(() => {
+        this.display = false
+      }, time)
+    },
+    hide () {
+      clearTimeout(this.timer)
+      this.display = false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.common-fixed-toast {
+  position: fixed;
+  z-index: 5200;
+  left: 0;
+  top: 0;
+  //@include vueTrans(opacity .3s ease-in-out) {opacity: 0}
+  &-mask {
+    position: fixed;
+    left: 0;
+    top: 0;
+  }
+  &-icon {
+    @include wh(80);
+    display: block;
+    margin: 0 auto;
+    &.tick {
+      @include bg(url('img/tick.svg'));
+    }
+    &.cross {
+      @include bg(url('img/cross.svg'));
+    }
+    &.warn {
+      @include bg(url('img/warn.svg'));
+    }
+  }
+  &-msg {
+    background-color: rgba(12, 12, 12, .7);
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    display: inline-block;
+    max-width: 4.28rem;
+    padding: .24rem;
+    overflow-wrap: break-word;
+    text-align: center;
+    border-radius: .16rem;
+    font-size: .3rem;
+    color: #fff;
+    &.sp {
+      padding: .7rem .8rem;
+      .ppw-toast-content {
+        //margin-top: .16rem;
+      }
+    }
+  }
+}
+</style>
---
+++ components/select/index.vue
@@ -0,0 +1,223 @@
+<template>
+  <div class="comp-select">
+    <el-select disabled class="comp-select-form" type="text" :placeholder="placeholder"  :value="lableVal"/>
+    <div class="open" @click="toastShow = true"></div>
+    <transition name="toast">
+      <common-flex justify="center" align="center" class="toast" v-if="toastShow">
+        <div class="toast-body">
+          <common-flex class="toast-body-title" justify="space-between" align="center">
+            <div>{{ placeholder }}</div>
+            <img src="./close.png" alt="" @click="close">
+          </common-flex>
+          <div class="toast-body-search" v-if="parentList.length > 7">
+            <input type="text" v-model="keyword" placeholder="请输入关键字搜索">
+            <img src="./search.png" alt="">
+          </div>
+          <div class="toast-body-list">
+            <common-flex justify="space-between" @click.native="choose(i)" :key="i.label" align="center" class="item" v-for="i of filterList">
+              <span v-html="toMark(i.label)"></span>
+              <img v-if="i.children" src="./right.png" alt="">
+            </common-flex>
+          </div>
+        </div>
+      </common-flex>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "comp-select",
+  model: {
+    prop: 'value',
+    event: 'change'
+  },
+  props: {
+    placeholder: String,
+    selector: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      lableVal: '',
+      showChild: false,
+      toastShow: false,
+      parentLabel: '',
+      parentVal: '',
+      keyword: '',
+      childList: []
+    }
+  },
+  computed: {
+    parentList () {
+      return this.showChild ? this.childList : this.selector
+    },
+    filterList () {
+      return this.parentList.filter((i) => i.label.indexOf(this.keyword) !== -1)
+    },
+  },
+  methods: {
+    toMark (str) {
+      if (this.keyword) return str.split(this.keyword).join(`<span style="color: red">${this.keyword}</span>`)
+      else return str
+    },
+    close () {
+      this.toastShow = false
+      this.keyword = ''
+      this.showChild = false
+      this.parentLabel = ''
+      this.parentVal = ''
+    },
+    choose (i) {
+      console.log(i)
+      if (i.children) {
+        this.showChild = true
+        this.childList = i.children
+        this.parentVal = i.value
+        this.parentLabel = i.label
+      } else {
+        if (this.showChild) {
+          this.lableVal = `${this.parentLabel}>${i.label}`
+          let value = `${this.parentVal}>${i.value}`
+          this.$emit('change', value)
+        } else {
+          this.lableVal = i.label
+          this.$emit('change', i.value)
+        }
+        this.close()
+      }
+    }
+  }
+
+}
+</script>
+
+<style lang="scss">
+.comp-select {
+  position: relative;
+  width: 100%;
+  height: 1rem;
+  .toast-enter, .toast-leave-to {
+    opacity: 0;
+    transform: translateY(-.1rem);
+  }
+  .toast-enter-active, .toast-leave-active {
+    transition: all linear .2s;
+  }
+  &-form {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 1rem;
+    border-radius: .08rem;
+    font-size: .3rem;
+    color: #333;
+    background-color: #fff;
+  }
+  .el-select {
+    position: absolute;
+    .el-input {
+      flex-shrink: 0;
+      width: 100%;
+      font-size: .3rem;
+      color: #333;
+      input {
+        height: 1rem;
+        background: #fff;
+        color: #333;
+        border: .02rem solid #D7DAE2;
+        &:disabled {
+          background: #fff;
+          color: #333;
+        }
+        &::placeholder {
+          font-size: .3rem;
+          color: #999;
+        }
+      }
+    }
+  }
+  .open {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 1rem;
+  }
+  .toast {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(#000, .3);
+    z-index: 10;
+    &-body {
+      padding-bottom: .3rem;
+      width: 6.7rem;
+      max-height: 9.8rem;
+      background-color: #fff;
+      border-radius: .2rem;
+      &-title {
+        padding: 0 .3rem;
+        height: .8rem;
+        font-size: .3rem;
+        color: #701D1D;
+        background: linear-gradient(90deg, #FFBD61, #FFF8A7, #FFBD61);
+        box-shadow: 0 .03rem .04rem 0 rgba(255, 255, 255, 0.5);
+        border-radius: .2rem .2rem 0 0;
+        img {
+          width: .27rem;
+          height: .27rem;
+        }
+      }
+      &-search {
+        position: relative;
+        padding: 0 .34rem .1rem;
+        input {
+          width: 100%;
+          height: .7rem;
+          text-indent: .15rem;
+          background-color: #EEEEEE;
+          font-size: .3rem;
+          color: #333;
+          border-radius: 0.1rem;
+          border: 1px solid #ddd;
+          &::placeholder {
+            font-size: .3rem;
+            color: #999;
+          }
+        }
+        img {
+          position: absolute;
+          right: .5rem;
+          top: .65rem;
+          width: .35rem;
+          height: .35rem;
+        }
+      }
+      &-list {
+        padding: 0 .3rem;
+        max-height: 6rem;
+        overflow-y: auto;
+        .item {
+          width: 100%;
+          height: .8rem;
+          font-size: .3rem;
+          color: #333;
+          img {
+            width: .26rem;
+            height: .2rem;
+          }
+        }
+      }
+    }
+  }
+
+}
+</style>
