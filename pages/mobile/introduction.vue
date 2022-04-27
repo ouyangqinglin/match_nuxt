ef31c22f6a778313f77b158bf1b34973f8dbf63e
yangqinglin <ouyangqinglinloveme@gmail.com>
2022-04-27 08:51:50 +0800

    fix(): H5路由跳转测试

--- pages/mobile/introduction.vue
+++ pages/mobile/introduction.vue
@@ -3,12 +3,13 @@
     <img :src="mobileConfig.index_banner" alt="">
     <common-flex justify="space-between" align="center" class="mobile-introduction-nav">
       <template v-for="(i, j) of menu">
-        <a @click="routeLink(i)">
+<!--        @click="routeLink(i)"-->
+        <nuxt-link :to="{path: i.route, query: {match_code: match_code, channel: channel, title: i.name}}">
           <common-flex direction="column" align="center" justify="center" class="mobile-introduction-nav-item">
             <img :src="i.icon" alt="">
             <div>{{ i.name }}</div>
           </common-flex>
-        </a>
+        </nuxt-link>
       </template>
     </common-flex>
     <div class="mobile-introduction-blank"  v-for="(i, j) of currPage.content">
@@ -42,6 +43,9 @@ export default {
     }),
     currPage () {
       return this.mobilePage.find(i => i.title.includes('简介'))
+    },
+    channel () {
+      return this.$route.query.channel || ''
     }
   },
   methods: {
