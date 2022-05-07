341b554587e861b7e55b0f4f15370245965ac7cd
yangqinglin <ouyangqinglinloveme@gmail.com>
2022-05-07 09:52:47 +0800

    style()：英大证券修改

--- pages/index/rank.vue
+++ pages/index/rank.vue
@@ -49,8 +49,8 @@
                 <template v-if="logined">
                   <!--                已认证-->
                   <template v-if="certificated">
-                    <div class="td ellipsis" v-if="j.prop === 'ret'" v-profit_handler.percent="i[j.prop]" />
-                    <div class="td ellipsis" v-else-if="j.prop === 'score'">{{ (i[j.prop] + '').slice(0, 5) }}</div>
+                    <div class="td ellipsis score" v-if="j.prop === 'ret'" v-profit_handler.percent="i[j.prop]" />
+                    <div class="td ellipsis score" v-else-if="j.prop === 'score'">{{ (i[j.prop] + '').slice(0, 5) }}</div>
                   </template>
                   <!--                未认证-->
                   <template v-else>
